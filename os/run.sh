b94892f730623a5a10b81a7ad124c4e14cf1cea1
tsh <you@example.com>
2023-01-13 19:27:59 +0800

    实现print和关机功能

---
+++ os/run.sh
@@ -0,0 +1,8 @@
+cargo build --release &&
+rust-objcopy --strip-all target/riscv64gc-unknown-none-elf/release/os -O binary target/riscv64gc-unknown-none-elf/release/os.bin &&
+qemu-system-riscv64 \
+    -machine virt \
+    -nographic \
+    -bios ../bootloader/rustsbi-qemu.bin \
+    -device loader,file=target/riscv64gc-unknown-none-elf/release/os.bin,addr=0x80200000
+
